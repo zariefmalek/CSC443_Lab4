b5c5a991686546f7c50a77488afc0929f569fac2
zariefmalek <2020462506@student.uitm.edu.my>
2022-06-08 00:17:46 +0800

    handle_calculator.php

---
+++ handle_calculator.php
@@ -0,0 +1,39 @@
+<html> 
+    <head> 
+        <title>Cost Calculator</title> 
+    </head>
+    <body> 
+     <?php #script handle calculator
+      ?> 
+
+if (isset($source)) {
+
+if ($source=calculator.html') {
+
+if ((is_numeric($quantity))) {
+
+(Staxrate +1);
+
+$total number_format ($total,2);
+
+echo "You are purchasing <b>".$_POST["quantity"],
+
+"</b> widgets at a cost of <b>". $_POST["price"] .
+
+"</b> each. With tax the total comes to <b>". Stotal. "</b>.\n";
+
+}else{
+
+echo "<p><b>Please enter a valid quantity to purchase!</b></p>";
+
+}else{
+
+echo "<p><b>You have accessed this page inappropriately!</b></p>";
+
+}else{
+
+echo "<p><b>You have accessed this page inappropriately!</b></p>";
+
+</body> 
+</html>
+    
